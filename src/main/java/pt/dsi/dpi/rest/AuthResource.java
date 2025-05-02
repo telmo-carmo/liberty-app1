4508aeb1a2737cb00d85437ea7998ad74eca09f4
Telmo Carmo <telmo.carmo@gmail.com>
2025-05-02 13:13:57 +0100

    no JWT feature used!

--- src/main/java/pt/dsi/dpi/rest/AuthResource.java
+++ src/main/java/pt/dsi/dpi/rest/AuthResource.java
@@ -25,45 +25,84 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
-//import org.eclipse.microprofile.jwt.Claims;
-//import org.eclipse.microprofile.jwt.Jwt;
-//import org.eclipse.microprofile.jwt.JwtBuilder;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+import java.security.Key;
 import java.time.Instant;
-import java.util.Arrays;
-import java.util.HashSet;
+import java.time.temporal.ChronoUnit;
 import java.util.logging.Logger;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Level;
 
 @Path("auth")
 public class AuthResource {
     private static final Logger logger = Logger.getLogger(AuthResource.class.getName());
 
-    //@Inject
-    //private JwtBuilder jwtBuilder;
+    @Inject
+    @ConfigProperty(name = "app.jwt.duration", defaultValue = "3600") // 1 hour default
+    long jwt_duration;
+
+    @Inject
+    @ConfigProperty(name = "app.jwt.secret", defaultValue = "my-secret-symmetric-key in 32 BY") // 256 bit key
+    String jwt_Secret;
 
     @POST
     @Path("login")
-    @Produces(MediaType.TEXT_PLAIN)
-    public String generateToken(
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response login(
             @QueryParam("username") String username,
             @QueryParam("password") String password) {
         logger.info("Generating token for user: " + username);
-        /* 
-        Instant now = Instant.now();
-        Instant expiry = now.plusSeconds(3600); // Token expires in 1 hour
+        String token = null;
+        String u_uid = null;
+        if ("123".equals(password))
+            u_uid = "1";
 
-        String token = jwtBuilder.claim(Claims.iss.name(), "my-issuer")
-                               .claim("upn", "bob") // Custom claim for username
-                               .claim("groups", new HashSet<>(Arrays.asList("user", "admin")))
-                               .issuedAt(now)
-                               .expiresAt(expiry)
-                               .subject("bob")
-                               .build();
-        */
-        String token = "NONE!"; 
-        return token;
+        if (u_uid != null) {
+            Instant expirationTime = Instant.now().plus(jwt_duration * 1000, ChronoUnit.MILLIS);
+            Date expirationDate = Date.from(expirationTime);
+            logger.info("JWT expires at " + expirationDate);
+            // SecretKey key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secretString));
+            Key key = Keys.hmacShaKeyFor(jwt_Secret.getBytes());
+            String bp_audience = "https://wwwcert.bportugal.net/adfs/oauth2/token";
+            //Collection<String> audienceList = new ArrayList<>();
+            //audienceList.add(bp_audience);
+
+            token = Jwts.builder()
+                    .claim("id", u_uid)
+                    .claim("sub", username)
+                    .claim("roles", new String[]{"user", "admin"})
+                    .expiration(expirationDate)
+                    .claim("aud", bp_audience)
+                    .signWith(key)
+                    .compact();
+        } else {
+            logger.log(Level.SEVERE,"Invalid login credentials");
+            return Response.status(Response.Status.UNAUTHORIZED).entity("Invalid credentials").build();
+        
+        }
+        Map<String, String> map1 = new HashMap<>();
+
+        map1.put("username", "demo");
+        if (token != null) {
+            map1.put("token", "Bearer " + token);
+            map1.put("error", null);
+        } else {
+            map1.put("token", null);
+            map1.put("error", "Error generating JWT");
+        }
+        logger.info(String.format("AuthResource.login JWT token=%s", token));
+        
+        return Response.ok(map1).build();
     }
 
     @GET
